2e5bba8275c6a046292b6ed4bbdaeeb46cb0b062
sweatystu <41545864+sweatystu@users.noreply.github.com>
2018-10-21 23:02:21 +0100

    Skills
    Immplemented script to take skills from skills.js file and display them in the left column as progress bars. May need to update formatting.

---
+++ skills.js
@@ -0,0 +1,14 @@
+var skills = [
+    {
+        name: "Tableu"
+        , level: "80"
+    }
+    , {
+        name: "QlikView"
+        , level: "75"
+    }
+    , {
+        name: "Excel"
+        , level: "95"
+    }
+];
