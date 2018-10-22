c5305fdaf34f21b63b6f61311e7f361fe1dfac11
sweatystu <41545864+sweatystu@users.noreply.github.com>
2018-10-22 17:56:49 +0100

    Completion
    Completed skills section. Includes handling nulls and values outside of limits.

--- skills.js
+++ skills.js
@@ -1,14 +1,33 @@
+/*
 var skills = [
     {
-        name: "Tableu"
-        , level: "80"
+        name: "Skill Name"
+        , level: "Skill Level"
+    },
+    {
+        name: "Skill 2"
+        , level: "Level 2"
     }
+];
+
+name is required. If level is null, it will be set to 50.
+Skill Level must be an integer number, between 50 and 100.
+Levels below 50 will be shown as 50.
+Levels above 100 will be shown as 100.
+Skills are shown in the order defined in this object.
+
+*/
+var skills = [
+    {
+        name: "Excel"
+        , level: "95"
+    }
+
     , {
         name: "QlikView"
         , level: "75"
     }
     , {
-        name: "Excel"
-        , level: "95"
+        name: "Tableau"
     }
 ];
