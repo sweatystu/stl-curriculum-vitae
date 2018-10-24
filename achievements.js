db9308be58b16d6fcac9447f3ea73f8af8247eeb
sweatystu <41545864+sweatystu@users.noreply.github.com>
2018-10-24 22:29:25 +0100

    Further Update
    Creation of more external files. Completion of Courses external file. Update to Skills external file.

---
+++ achievements.js
@@ -0,0 +1,22 @@
+/*
+var achievements = [
+    {
+        company: "Where was this feat done?"
+        , what: "What did I do?"
+        , by: "What did I do to achieve it?"
+        , result: "what was the outcome?"
+        , comparison: "How does this outcome compare?"
+    }
+]
+*/
+
+var achievements = [
+    {
+        company: "Infineum"
+        , what: "Improved the analysis of tribological testing"
+        , by: "Learning and implementing VBA"
+        , result: "Improved anaysis time of ~ 5 seconds"
+        , comparison: "Previous analysis time of ~ 10 minutes"
+    }
+];
+
